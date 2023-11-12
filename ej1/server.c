66377616fda0af1afd446ba4676c3dc8d03fc981
spb49-ua <spb49@alu.ua.es>
2023-11-12 13:44:27 +0100

    ej1 terminado

---
+++ ej1/server.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 9999
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    FILE *file;
+
+    // Crear descriptor de archivo de socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Asignar el socket al puerto 9999
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Vincular el socket al puerto 9999
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // Enviar archivo "Google.html"
+    file = fopen("Google.html", "rb");
+    if (file == NULL) {
+        perror("File not found");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        send(new_socket, buffer, strlen(buffer), 0);
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    printf("File sent successfully\n");
+
+    fclose(file);
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
